0bcd6e4e0cfa8f494aa08ee84fa3afd7ffbfc7ef
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-08-16 16:20:22 +0300

    Новая структура проекта

---
+++ src/statistics/min.cpp
@@ -0,0 +1,13 @@
+#include "min.h"
+
+Min::Min() : m_min{std::numeric_limits<double>::min()} {}
+
+void Min::update(double next) {
+  if (next < m_min) {
+    m_min = next;
+  }
+}
+
+double Min::eval() const { return m_min; }
+
+const char *Min::name() const { return "min"; }
---
+++ src/statistics/min.h
@@ -0,0 +1,17 @@
+#ifndef MIN_H
+#define MIN_H
+
+#include "istatistics.h"
+#include <limits>
+
+class Min : public IStatistics {
+public:
+  Min();
+  void update(double next) override;
+  double eval() const override;
+  const char *name() const override;
+
+private:
+  double m_min;
+};
+#endif // MIN_H
