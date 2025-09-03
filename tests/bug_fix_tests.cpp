9bc4dd83e142e1225fed1154eb5b1d61517fbb43
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2025-09-03 22:07:43 +0300

    Пробные тесты

---
+++ tests/bug_fix_tests.cpp
@@ -0,0 +1,40 @@
+// @file bug_fix_tests.cpp
+#include <gtest/gtest.h>
+#include <sstream>
+#include <cmath>
+
+#include "../src/statistics/istatistics.h"
+#include "../src/statistics/max.h"
+#include "../src/statistics/min.h"
+#include "../src/statistics/mean.h"
+#include "../src/statistics/std.h"
+#include "../src/statistics/pst.h"
+
+// 1. Вывод может быть неожиданным для пользователя нашего класса: 
+// std::cout << Min{}.eval() << std::endl; Мелочь, не оговоренная явно в задании, 
+// но всегда лучше обрабатывать граничные случаи.  
+TEST(BugFixTests, Bug1) {
+    Min min;
+    std::cout << "Initial min: " << min.eval() << std::endl;
+    // Проверяем, что до обновления значение eval() возвращает NaN
+    EXPECT_TRUE(std::isnan(min.eval()));
+}
+
+// 2.Вывод этого кода обескураживает, точно стоит исправить: 
+// Max max;
+// max.update(0);
+// std::cout << "max: " << max.eval() << std::endl; 
+TEST(BugFixTests, Bug2) {
+    Max max;
+    max.update(0);
+    EXPECT_DOUBLE_EQ(max.eval(), 0.0);
+} 
+
+// 3. В Std можно переиспользовать реализацию Mean. 
+// Mean mean;
+// std::cout << mean.eval() << std::endl;  
+
+
+// 4. Расчет varianceSum и т.п. лучше проводить непосредственно в eval, 
+// т.к. в общем случае логично предположить, что eval вызывается реже 
+// update и сейчас часть вычислений игнорируется. 
